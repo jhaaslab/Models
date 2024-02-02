d9acefab4b36ec8da175545115af2fc0c0b832c1
Austin <AustinJMendoza@proton.me>
2024-02-02 16:54:06 -0500

    added latest benchmarks

--- benchmarks/modelBenchmark.jl
+++ benchmarks/modelBenchmark.jl
@@ -1,112 +1,150 @@
-#
-include("TRN_network.jl")
-
-using .TRN_network
-using OrdinaryDiffEq
-using MAT
-
-namesOfNeurons = ["TRN1","TRN2","TRN3","TRN4","TRN5","TRN6","TRN7","TRN8","TRN9",
-                  "TC1", "TC2", "TC3", "TC4", "TC5", "TC6", "TC7", "TC8", "TC9" ]
-startTime  = 0.0
-endTime    = 1000.0
-tspan = (startTime, endTime)
-
-reps = 100;
-
-tlocal = zeros(9)
-
-for n = 1:9
-    n_names = [namesOfNeurons[1:n]; namesOfNeurons[10:9+n]]
-    numNeurons = length(n_names)
-
-    ## Initial conditions
-    u0, per_neuron = initialconditions(numNeurons,false)
-
-
-    tlocal[n] = @elapsed Threads.@threads for i = 1:reps
-        # Initialize simParams
-        p = simParams(names=n_names,n=numNeurons,per_neuron=per_neuron)
-
-        # Vars 
-        ## Inputs
-        for ii = 1:p.n
-            #= DC
-            p.bias[ii] = 0.3
-            =#
-            p.iDC[ii] = 1.0
-            p.iStart[ii] = 200.0
-            p.iStop[ii] = 800.0
-            #= noise
-            p.A[ii] = 0.2
-            p.tA[ii] = poissonP(80,endTime)
-            p.AI[ii] = 0.2
-            p.tAI[ii] = poissonP(20,endTime)
-            =#
-        end
-
-        prob = ODEProblem(dsim!, u0, tspan, p)
-
-        # Start sim
-        sol = solve(prob, VCAB3(), saveat=0.1, save_idxs=1:p.per_neuron:length(u0))
-
-    end
-end
-
-matwrite(string(pwd(),"\\exec_time_net_nonoise.mat"),Dict("tlocal"=>tlocal))
-
 #=
-include("TRN_simple.jl")
-
-using .TRN_simple
+using Models.TRNmodel
 using OrdinaryDiffEq
 using MAT
 
-namesOfNeurons = ["TRN1","TRN2","TRN3","TRN4","TRN5","TRN6","TRN7","TRN8","TRN9",
-         "TRN10","TRN11","TRN12","TRN13","TRN14","TRN15","TRN16","TRN17","TRN18"]
+
+function main()
 startTime  = 0.0
 endTime    = 1000.0
 tspan = (startTime, endTime)
+numNeurons = [collect(2:2:18); 100]
 
-reps = 100;
+reps = 100
 
-tlocal = zeros(9)
+tlocal = zeros(length(numNeurons))
 
-for n = 1:9
-    n_names = namesOfNeurons[1:n*2]
-    numNeurons = length(n_names)
+for n in eachindex(numNeurons)
+    namesOfNeurons = ["TRN$i" for i in 1:numNeurons[n]]
 
     ## Initial conditions
-    u0, per_neuron = initialconditions(numNeurons,false)
+    u0, per_neuron = initialconditions(numNeurons[n],false)
 
+    ## Initialize simParams
+    Params = Vector{simParams}(undef,reps)
 
-    tlocal[n] = @elapsed Threads.@threads for i = 1:reps
-        # Initialize simParams
-        p = simParams(names=n_names,n=numNeurons,per_neuron=per_neuron)
+    for i = 1:reps
+        p = simParams(names=namesOfNeurons,n=numNeurons[n],per_neuron=per_neuron)
 
-        # Vars 
+        # Vars
         ## Inputs
         for ii = 1:p.n
-            #= DC
-            p.bias[ii] = 0.3
-            =#
             p.iDC[ii] = 1.0
-            p.iStart[ii] = 200.0
-            p.iStop[ii] = 800.0
-            #= noise
-            p.A[ii] = 0.2
-            p.tA[ii] = poissonP(80,endTime)
-            p.AI[ii] = 0.2
-            p.tAI[ii] = poissonP(20,endTime)
-            =#
+            p.iStart[ii] = [200.0]
+            p.iStop[ii]  = [800.0]
         end
+
+        Params[i] = p
+    end
+
+    tlocal[n] = @elapsed Threads.@threads for i = 1:reps
+        p = Params[i]
 
         prob = ODEProblem(dsim!, u0, tspan, p)
 
         # Start sim
-        sol = solve(prob, VCAB3(), saveat=0.1, save_idxs=1:p.per_neuron:length(u0))
-
+        solve(prob,BS3(),save_everystep=false,save_idxs=1:p.per_neuron:length(u0))
     end
 end
 
-matwrite(string(pwd(),"\\exec_time_simple_nonoise.mat"),Dict("tlocal"=>tlocal))
+matwrite(joinpath(pwd(),"texec_model_nonoise.mat"),Dict("tlocal"=>tlocal))
+
 =#
+
+using Models.TC_TRNnetwork
+using OrdinaryDiffEq
+using MAT
+
+function main()
+startTime  = 0.0
+endTime    = 1000.0
+tspan = (startTime, endTime)
+numNeurons = [collect(2:2:18); 100]
+
+reps = 100
+
+tlocal = zeros(length(numNeurons))
+
+for n in eachindex(numNeurons)
+    namesOfNeurons = [["TRN$i" for i in 1:div(numNeurons[n],2)]; ["TC$i" for i in 1:div(numNeurons[n],2)]]
+    ## Initial conditions
+    u0, per_neuron = initialconditions(numNeurons[n])
+
+    ## Initialize simParams
+    Params = Vector{simParams}(undef,reps)
+
+    for i = 1:reps
+        p = simParams(names=namesOfNeurons,n=numNeurons[n],per_neuron=per_neuron)
+
+        # Vars
+        ## Inputs
+        for ii = 1:p.n
+            p.bias[ii] = 0.3
+            # noise
+            p.A[ii] = 0.5
+            p.tA[ii] = poissonP(80,endTime)
+            p.AI[ii] = 0.5
+            p.tAI[ii] = poissonP(20,endTime)
+        end
+
+        Params[i] = p
+    end
+
+    tlocal[n] = @elapsed Threads.@threads for i = 1:reps
+        p = Params[i]
+
+        prob = ODEProblem(dsim!, u0, tspan, p)
+
+        # Start sim
+        solve(prob,BS3(),save_everystep=false,save_idxs=1:p.per_neuron:length(u0))
+    end
+end
+
+matwrite(joinpath(pwd(),"texec_TC_TRNnet_noise.mat"),Dict("tlocal"=>tlocal))
+
+
+
+
+tlocal = zeros(length(numNeurons))
+
+for n in eachindex(numNeurons)
+    namesOfNeurons = [["TRN$i" for i in 1:div(numNeurons[n],2)]; ["TC$i" for i in 1:div(numNeurons[n],2)]]
+
+    ## Initial conditions
+    u0, per_neuron = initialconditions(numNeurons[n],false)
+
+    ## Initialize simParams
+    Params = Vector{simParams}(undef,reps)
+
+    for i = 1:reps
+        p = simParams(names=namesOfNeurons,n=numNeurons[n],per_neuron=per_neuron)
+
+        # Vars
+        ## Inputs
+        for ii = 1:p.n
+            p.iDC[ii] = 1.0
+            p.iStart[ii] = [200.0]
+            p.iStop[ii]  = [800.0]
+        end
+
+        Params[i] = p
+    end
+
+    tlocal[n] = @elapsed Threads.@threads for i = 1:reps
+        p = Params[i]
+
+        prob = ODEProblem(dsim!, u0, tspan, p)
+
+        # Start sim
+        solve(prob,BS3(),save_everystep=false,save_idxs=1:p.per_neuron:length(u0))
+    end
+end
+
+matwrite(joinpath(pwd(),"texec_TC_TRNnet_nonoise.mat"),Dict("tlocal"=>tlocal))
+
+
+nothing
+end #main
+
+main()
+
